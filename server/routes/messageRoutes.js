d231f309370a49929a0ff556222bb5b02144f35a
Aditya Coder <coderaditya41@gmail.com>
2025-05-23 12:34:27 +0530

    sever.js updated for vercel

--- server/routes/messageRoutes.js
+++ server/routes/messageRoutes.js
@@ -1,17 +1,12 @@
-// server/routes/messageRoutes.js
 import express from "express";
-// deleteMessage को इम्पोर्ट करें
-import { getMessages, getUsersForSidebar, markMessageAsSeen, sendMessage, deleteMessage } from "../controllers/messageController.js";
 import { protectRoute } from "../middleware/auth.js"; // .js extension added
+import { getMessages, getUsersForSidebar, markMessageAsSeen, sendMessage } from "../controllers/messageController.js"; // .js extension added
 
 const messageRouter = express.Router();
 
 messageRouter.get("/users", protectRoute, getUsersForSidebar);
 messageRouter.get("/:id", protectRoute, getMessages);
-messageRouter.get("/mark/:id", protectRoute, markMessageAsSeen); // Note: Typically mark as seen would be a PUT request
+messageRouter.get("/mark/:id", protectRoute, markMessageAsSeen);
 messageRouter.post("/send/:id", protectRoute, sendMessage);
 
-// --- नया DELETE रूट ---
-messageRouter.delete("/delete/:id", protectRoute, deleteMessage); // New route for deleting messages
-
-export default messageRouter;
+export default messageRouter; // .js extension added
